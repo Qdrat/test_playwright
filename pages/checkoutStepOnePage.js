e3560277a11bc9c00392d8ece9d03caa92b453f0
Qdrat <alfredhager0420@gmail.com>
2026-04-05 00:33:01 +0300

    add all test

---
+++ pages/checkoutStepOnePage.js
@@ -0,0 +1,18 @@
+export class CheckoutStepOnePage{
+    constructor(page){
+        this.page = page;
+
+        this.firstNameInput = page.locator('[data-test="firstName"]');
+        this.lastNameInput = page.locator('[data-test="lastName"]');
+        this.postalCodeInput = page.locator('[data-test="postalCode"]');
+        this.buttonContinue = page.locator('.submit-button');
+    }
+
+    async fillUserInfo(firstName, lastName, postalCode){
+        await this.firstNameInput.fill(firstName);
+        await this.lastNameInput.fill(lastName);
+        await this.postalCodeInput.fill(postalCode);
+        
+        await this.buttonContinue.click();
+    }
+}
